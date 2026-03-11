48e0cdf8c3770d989cbc5a4f9da17bec17e2cef8
josephine-vargas <josephine.vargas1@upr.edu>
2026-03-11 19:30:30 -0400

    Enemy Positions

--- src/Program.cpp
+++ src/Program.cpp
@@ -17,8 +17,8 @@ Program::Program() {
         });
 
     for (int i = 0; i < 30; i++) {
-        float x = 250 + 50 * i;
-        float y = 200 + 50 * i;
+        float x = 250 + 50 * (i % 10);
+        float y = 200 + 50 * (i/10);
 
         Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
             std::pair<float, float>{x, y}, 
